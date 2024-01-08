fcd787db86a074e388d0f69b53389b334238e82f
ariwa23 <97737739+ariwa23@users.noreply.github.com>
2024-01-08 18:57:45 +0700

    contoh judul

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>INVITATION</title>
+</head>
+<body>
+  <h1>REGARDS</h1>
+
+  <image controls>
+    <source src="C:\xampp\htdocs\Employe\aset\tes.jpg" type = image/jpg>
+</image>
+
+
+  <video controls>
+    <source src="https://www.youtube.com/watch?v=o7W-u1fPmnE">
+  </video>
+</body>
+</html>
